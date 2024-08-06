0ae4371f320373d4359d4e4bb0293a36cba49cd4
Sofa <muhammadsofa52@yahoo.com>
2024-08-07 06:37:34 +0700

    displays data from the database

---
+++ app/Http/Controllers/PasienController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PasienController extends Controller
+{
+    public function index()
+    {
+        $data['pasien'] = \App\Models\Pasien::latest()->paginate(10);
+        return view('pasien_index', $data);
+    }
+}
---
+++ resources/views/pasien_index.blade.php
@@ -0,0 +1,35 @@
+@extends('mylayout', ['title' => 'Data Pasien'])
+@section('content')
+     <div class="card">
+          <div class="card-body">
+               <h3>Data Pasien</h3>
+               <table class="table table-striped">
+                    <thead>
+                         <tr>
+                              <th>NO</th>
+                              <th>No Pasien</th>
+                              <th>Nama</th>
+                              <th>Umur</th>
+                              <th>Jenis Kelamin</th>
+                              <th>Tanggal Buat</th>
+                              <th>Aksi</th>
+                         </tr>
+                    </thead>
+                    <tbody>
+                         @foreach($pasien as $item)
+                              <tr>
+                                   <td>{{ $loop->iteration }}</td>
+                                   <td>{{ $item->no_pasien }}</td>
+                                   <td>{{ $item->nama }}</td>
+                                   <td>{{ $item->umur }}</td>
+                                   <td>{{ $item->jenis_kelamin }}</td>
+                                   <td>{{ $item->created_at }}</td>
+                                   <td></td>
+                              </tr>
+                         @endforeach
+                    </tbody>
+               </table>
+               {!! $pasien->links() !!}
+          </div>
+     </div>
+@endsection
